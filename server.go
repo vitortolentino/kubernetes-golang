3280f445ad2917eb7f656bfff8437431a5e4582a
vitortolentino <vitorafonso33@gmail.com>
2022-02-10 08:03:24 -0300

    feat: updating docker version

--- server.go
+++ server.go
@@ -8,5 +8,5 @@ func main() {
 }
 
 func HelloHandler(response http.ResponseWriter, request *http.Request) {
-	response.Write([]byte("<h1>Hello World</h1>"))
+	response.Write([]byte("<h1>Hello World!!!</h1>"))
 }
